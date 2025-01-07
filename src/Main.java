bcd922cf65d8e5e341c9dd8fb83191b68b85c1e5
NadaAlaa299 <nadaalaa2909@gmail.com>
2025-01-07 20:52:21 +0200

    write the code in main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true)
+        {
+            System.out.println("\n< Shape Calculator Program >");
+            System.out.println("1) Calculate Circle");
+            System.out.println("2) Calculate Rectangle");
+            System.out.println("3) Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+            System.out.print("\n");
+            switch (choice){
+                case 1:
+                    System.out.print("Enter circle radius: ");
+                    double radius = scanner.nextDouble();
+                    Shape circle = new Circle(radius);
+                    circle.printDetails();
+                    break;
+                case 2:
+                    System.out.print("Enter the length of the rectangle: ");
+                    double length = scanner.nextDouble();
+                    System.out.print("Enter the width of the rectangle: ");
+                    double width = scanner.nextDouble();
+                    Shape rectangle = new Rectangle(length, width);
+                    rectangle.printDetails();
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice, Please try again.");
+            }
+        }
     }
 }
