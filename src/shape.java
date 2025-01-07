66488c4ac8779553b788be5d8076b39082064f41
NadaAlaa299 <nadaalaa2909@gmail.com>
2025-01-07 20:19:44 +0200

    create shape class

---
+++ src/shape.java
@@ -0,0 +1,10 @@
+public abstract class shape {
+    public abstract double cal_area();
+    public abstract double cal_perimeter();
+
+    public void printDetails()
+    {
+        System.out.println("Area = " + cal_area());
+        System.out.println("Perimeter = " + cal_perimeter());
+    }
+}
